824ae5bd82df0a5d760cff59c6f7308033f4f107
CISEL <76199462+cisel-dev@users.noreply.github.com>
2021-02-02 16:09:56 +0100

    Update karbon-app-mon-setup-demo.sh

--- karbon-app-mon-setup-demo.sh
+++ karbon-app-mon-setup-demo.sh
@@ -4,7 +4,7 @@
 kubectl label ns/kube-system monitoring=k8s
 kubectl label ns/ntnx-system monitoring=k8s
 
-# Patch existing prometheus resource to limit ServiceMonitors used
+# Patch existing prometheus resource to limit ServiceMonitors to kube-system and ntnx-system namespaces
 kubectl -n ntnx-system patch --type merge prometheus/k8s -p '{"spec":{"serviceMonitorNamespaceSelector":{"matchLabels":{"monitoring": "k8s"}}}}'
 kubectl apply -f https://raw.githubusercontent.com/cisel-dev/karbon-app-monitoring-demo/main/karbon-app-mon-rbac-demo.yml
 kubectl apply -f https://raw.githubusercontent.com/cisel-dev/karbon-app-monitoring-demo/main/karbon-app-mon-prometheus-demo.yml
